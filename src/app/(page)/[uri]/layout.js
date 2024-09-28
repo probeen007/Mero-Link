6b8ef46df13f453ba3d2b1b797fa9355b79ac17a
Prabin bhattarai <122869241+probeen007@users.noreply.github.com>
2024-09-28 23:07:20 +0545

    Basic Seo and title logo change

--- src/app/(page)/[uri]/layout.js
+++ src/app/(page)/[uri]/layout.js
@@ -5,10 +5,18 @@ import '../../globals.css'
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata = {
-  title: 'Mero Link',
-  description: 'Mero Link is a startup from Nepal, designed to simplify how you share your online identity. Create a custom link tree that combines all your social media profiles, websites, and important links into one seamless link. This is Merolink user page',
-  keywords: 'Mero Link, custom link tree, online presence, social media, digital identity, startup Nepal, share links, unify profiles'
-}
+  title: 'Mero Link - Unify Your Online Presence with Ease',
+  description: 'Mero Link is a revolutionary startup from Nepal, designed to help you effortlessly unify your online identity. Create personalized link trees to combine all your social media profiles, websites, and essential links into one seamless, shareable link. Perfect for professionals, influencers, and businesses seeking to enhance their digital presence, Mero Link makes it easy to connect and share everything in one place. Join the Mero Link community today and take control of your online presence!',
+  keywords: 'Mero Link, custom link tree, online presence, social media, digital identity, startup Nepal, share links, unify profiles',
+  author: 'Prabin Bhattarai',
+  robots: 'index, follow',
+  og: {
+    title: 'Mero Link - Simplify Your Digital Presence with Custom Link Trees',
+    description: 'Join Mero Link, a startup from Nepal, to effortlessly combine your social media and essential links into one shareable tree. Perfect for individuals and businesses alike!',
+    url: 'https://merolink.me', // Replace with your app's URL
+    image: 'https://i.postimg.cc/cLBWw4xd/p.png', // Replace with a relevant image URL
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -16,8 +16,8 @@ export const metadata = {
   og: {
     title: 'Mero Link - Simplify Your Digital Presence with Custom Link Trees',
     description: 'Join Mero Link, a startup from Nepal, to effortlessly combine your social media and essential links into one shareable tree. Perfect for individuals and businesses alike!',
-    //url: 'https://yourappurl.com', // Replace with your app's URL
-    //image: 'https://yourappurl.com/image.jpg', // Replace with a relevant image URL
+    url: 'https://merolink.me', // Replace with your app's URL
+    image: 'https://i.postimg.cc/cLBWw4xd/p.png', // Replace with a relevant image URL
   },
 };
 
