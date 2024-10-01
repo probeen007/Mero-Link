54a3205871e8bf2ad72d2b1bd8ad069d08cf4c1d
Prabin bhattarai <122869241+probeen007@users.noreply.github.com>
2024-10-01 19:01:31 +0545

    improve metadata

--- src/app/(page)/[uri]/layout.js
+++ src/app/(page)/[uri]/layout.js
@@ -6,7 +6,7 @@ const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata = {
   title: 'Mero Link - Unify Your Online Presence with Ease',
-  description: 'Mero Link is a revolutionary startup from Nepal, designed to help you effortlessly unify your online identity. Create personalized link trees to combine all your social media profiles, websites, and essential links into one seamless, shareable link. Perfect for professionals, influencers, and businesses seeking to enhance their digital presence, Mero Link makes it easy to connect and share everything in one place. Join the Mero Link community today and take control of your online presence!',
+  description: 'Unify your online presence with Mero Link! Create personalized link trees for your social profiles and websites. Perfect for professionals and businesses.',
   keywords: 'Mero Link, custom link tree, online presence, social media, digital identity, startup Nepal, share links, unify profiles',
   author: 'Prabin Bhattarai',
   robots: 'index, follow',
